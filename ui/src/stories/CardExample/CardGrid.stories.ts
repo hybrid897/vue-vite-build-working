28798a0f6261d7476097b0cc9e2fc550c2424c94
Laurence Dean <laurencedevtest@gmail.com>
2024-04-13 21:31:14 +0800

    fix formatting in html :)

--- ui/src/stories/CardExample/CardGrid.stories.ts
+++ ui/src/stories/CardExample/CardGrid.stories.ts
@@ -1,28 +1,49 @@
 // CardGrid.stories.ts
-import CardGrid from './CardGrid.vue';
-import SingleCard from './SingleCard.vue';
-import Button from '../Button.vue';
-
+import CardGrid from './CardGrid.vue'
+import SingleCard from './SingleCard.vue'
+import Button from '../Button.vue'
+const html = String.raw
 export default {
   title: 'Components/CardGrid',
-  component: { CardGrid, SingleCard },
-};
+  component: { CardGrid, SingleCard }
+}
 
 export const Default: any = () => ({
   components: { CardGrid, SingleCard },
   setup() {
     // Simulated data
     const cards = [
-      { id: 1, title: 'Card 1', description: 'Description for card 1', image: 'https://via.placeholder.com/150' },
-      { id: 2, title: 'Card 2', description: 'Description for card 2', image: 'https://via.placeholder.com/150' },
-      { id: 3, title: 'Card 3', description: 'Description for card 3', image: 'https://via.placeholder.com/150' }
-    ];
+      {
+        id: 1,
+        title: 'Card 1',
+        description: 'Description for card 1',
+        image: 'https://via.placeholder.com/150'
+      },
+      {
+        id: 2,
+        title: 'Card 2',
+        description: 'Description for card 2',
+        image: 'https://via.placeholder.com/150'
+      },
+      {
+        id: 3,
+        title: 'Card 3',
+        description: 'Description for card 3',
+        image: 'https://via.placeholder.com/150'
+      }
+    ]
 
-    return { cards };
+    return { cards }
   },
-  template: `
-    <CardGrid>
-      <SingleCard v-for="card in cards" :key="card.id" :title="card.title" :description="card.description" :image="card.image" />
-    </CardGrid>
-  `,
-});
+  template: html`
+    <card-grid>
+      <single-card
+        v-for="card in cards"
+        :key="card.id"
+        :title="card.title"
+        :description="card.description"
+        :image="card.image"
+      />
+    </card-grid>
+  `
+})
--- ui/src/stories/CvaExample.stories.ts
+++ ui/src/stories/CvaExample.stories.ts
@@ -1,23 +1,24 @@
 import type { Meta, StoryObj } from '@storybook/vue3'
 import CvaExample from './CvaExample.vue'
 import Button from './Button.vue'
-import type { ComponentProps } from 'vue-component-type-helpers';
+import type { ComponentProps } from 'vue-component-type-helpers'
+const html = String.raw
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 
-type CvaButtonAndProps = ComponentProps<typeof CvaExample>;
+type CvaButtonAndProps = ComponentProps<typeof CvaExample>
 
 const meta = {
-   title: 'CvaButton',
+  title: 'CvaButton',
   parameters: { layout: 'centered' },
   component: CvaExample,
   // This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
-  argTypes: {    
+  argTypes: {
     intent: {
       control: 'select',
-      options: ['primary', 'secondary'],
-    },
-  },
+      options: ['primary', 'secondary']
+    }
+  }
 } satisfies Meta<CvaButtonAndProps>
 
 export default meta
@@ -28,30 +29,42 @@ type Story = StoryObj<typeof meta>
  * See https://storybook.js.org/docs/api/csf
  * to learn how to use render functions.
  */
-export const Primary: Story = {
-}
+export const Primary: Story = {}
 
 /*
  *👇 Render functions are a framework specific feature to allow you control on how the component renders.
  * See https://storybook.js.org/docs/api/csf
  * to learn how to use render functions.
  */
- export const UsingSlot: Story = {
+export const UsingSlot: Story = {
   render: (args) => ({
     components: { CvaExample },
     setup() {
       const cards = [
-        { id: 111, title: 'Card 1', description: 'Description for card 1', image: 'https://via.placeholder.com/150' },
-        { id: 222, title: 'Card 2', description: 'Description for card 2', image: 'https://via.placeholder.com/150' },
-        { id: 333, title: 'Card 3', description: 'Description for card 3', image: 'https://via.placeholder.com/150' }
+        {
+          id: 111,
+          title: 'Card 1',
+          description: 'Description for card 1',
+          image: 'https://via.placeholder.com/150'
+        },
+        {
+          id: 222,
+          title: 'Card 2',
+          description: 'Description for card 2',
+          image: 'https://via.placeholder.com/150'
+        },
+        {
+          id: 333,
+          title: 'Card 3',
+          description: 'Description for card 3',
+          image: 'https://via.placeholder.com/150'
+        }
       ]
-      return { args, cards };
+      return { args, cards }
     },
     // V-Bind necessary for args to bind to template correctly
-    template: `
-      <CvaExample
-        v-for="card in cards"
-        >
+    template: html`
+      <cva-example v-for="card in cards">
         <template #center>
           <span class="align-center mr-2">{{card.id}} {{card.title}}</span>
           <a
@@ -66,80 +79,91 @@ export const Primary: Story = {
             <button>{{ card.description }}</button>
           </a>
         </template>
-      </CvaExample>
+      </cva-example>
     `
   }),
-   decorators: [
+  decorators: [
     (story) => ({
       components: { story, Button },
-      template: '<Button><story /></Button>',
-    }),
-  ],
- }
+      template: '<Button><story /></Button>'
+    })
+  ]
+}
 
 /*
  *👇 Render functions are a framework specific feature to allow you control on how the component renders.
  * See https://storybook.js.org/docs/api/csf
  * to learn how to use render functions.
  */
- export const Secondary: Story = {
+export const Secondary: Story = {
   args: {
     center: 'SecondaryTest',
     intent: 'secondary'
-  },
- }
+  }
+}
 
- /*
+/*
  *👇 Render functions are a framework specific feature to allow you control on how the component renders.
  * See https://storybook.js.org/docs/api/csf
  * to learn how to use render functions.
  */
- export const AsLink: Story = {
-
+export const AsLink: Story = {
   args: {
     as: 'a',
     center: 'AsDangerLink',
     href: '#',
     intent: 'danger'
-  },
- }
+  }
+}
 
-  /*
+/*
  *👇 Render functions are a framework specific feature to allow you control on how the component renders.
  * See https://storybook.js.org/docs/api/csf
  * to learn how to use render functions.
  */
- export const Disabled: Story = {
-
+export const Disabled: Story = {
   args: {
     center: 'Disabled',
     disabled: true
-  },
- }
+  }
+}
 
- export const ButtonGrid: Story = {
+export const ButtonGrid: Story = {
   render: (args) => ({
     components: { CvaExample },
     setup() {
       const cards = [
-        { id: 1, title: 'Card 1', description: 'Description for card 1', image: 'https://via.placeholder.com/150' },
-        { id: 2, title: 'Card 2', description: 'Description for card 2', image: 'https://via.placeholder.com/150' },
-        { id: 3, title: 'Card 3', description: 'Description for card 3', image: 'https://via.placeholder.com/150' }
+        {
+          id: 1,
+          title: 'Card 1',
+          description: 'Description for card 1',
+          image: 'https://via.placeholder.com/150'
+        },
+        {
+          id: 2,
+          title: 'Card 2',
+          description: 'Description for card 2',
+          image: 'https://via.placeholder.com/150'
+        },
+        {
+          id: 3,
+          title: 'Card 3',
+          description: 'Description for card 3',
+          image: 'https://via.placeholder.com/150'
+        }
       ]
-      return { args, cards };
+      return { args, cards }
     },
     // V-Bind necessary for args to bind to template correctly
-    template: `
-    <template v-for="card in cards">
-      <CvaExample >
-        <template v-slot:center> 
-        {{ card.title}} SLOT
-        </template>
-      </CvaExample>
+    template: html`
+      <template v-for="card in cards">
+        <cva-example>
+          <template v-slot:center> {{ card.title }} SLOT </template>
+        </cva-example>
       </template>
-    `,
+    `
   }),
   args: {
-    center: 'Button Grid Arg',
-  },
- } 
+    center: 'Button Grid Arg'
+  }
+}
